1637e5290ce9a9ba7c416fffda13e6500086a0a1
dmiweb <dimbons@mail.ru>
2024-07-14 17:01:38 +0300

    upd readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Game Whack Release
 
+## GitHub Pages
+(https://dmiweb.github.io/game-whack-release/)
+
 [![Build status](https://ci.appveyor.com/api/projects/status/ivbjy8hvsfmrf16x?svg=true)](https://ci.appveyor.com/project/dmiweb/game-whack-release)
--- src/components/game-rendering/GameRendering.js
+++ src/components/game-rendering/GameRendering.js
@@ -7,20 +7,20 @@ export default class GameRendering {
   }
 
   gridGenerator() {
-    this._element.insertAdjacentHTML(
-      "beforeEnd",
-      `
-        <div class="scoreboard">
-          <div>Убито - <span class="dead">0</span></div>
-          <div>Пропущено - <span class="skipped">0</span></div>
-        </div>
-        <div class="container"></div>`,
-    );
+    const scoreBoard = `
+      <div class="scoreboard">
+        <div>Убито - <span class="dead">0</span></div>
+        <div>Пропущено - <span class="skipped">0</span></div>
+      </div>
+      <div class="container"></div>
+    `;
+
+    const gameGrid = `<div class="cell"></div>`;
+
+    this._element.insertAdjacentHTML("beforeEnd", scoreBoard);
 
     for (let i = 0; i < 16; i++) {
-      this._element
-        .querySelector(".container")
-        .insertAdjacentHTML("beforeEnd", '<div class="cell"></div>');
+      this._element.querySelector(".container").insertAdjacentHTML("beforeEnd", gameGrid);
     }
   }
 }
